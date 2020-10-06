9a3f85d357f4506deb4c7cc3e802dc64e061f313
Jessica Nash <janash@vt.edu>
2020-10-06 15:39:31 -0400

    run black - residue_report.py

--- ELECTRIC/residue_report.py
+++ ELECTRIC/residue_report.py
@@ -10,7 +10,7 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('pdb_file', help='The pdb file to analyze.')
+    parser.add_argument("pdb_file", help="The pdb file to analyze.")
 
     args = parser.parse_args()
 
