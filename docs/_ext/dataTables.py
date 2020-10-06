4497cc4e52782edb6bdc5df8bf0d793a89955583
Jessica Nash <janash@vt.edu>
2020-10-06 15:00:06 -0400

    run black on docs files

--- docs/_ext/dataTables.py
+++ docs/_ext/dataTables.py
@@ -13,6 +13,7 @@ from sphinx.util.docutils import SphinxDirective
 class data_table(nodes.General, nodes.Element):
     pass
 
+
 class DataTable(SphinxDirective):
 
     has_content = True
@@ -21,14 +22,14 @@ class DataTable(SphinxDirective):
         settings = {}
 
         for content in self.content:
-            split_content = content.split(':', 1)
-            settings[split_content[0].strip() ] = split_content[1].strip()
+            split_content = content.split(":", 1)
+            settings[split_content[0].strip()] = split_content[1].strip()
 
         node = data_table()
-        node.table_id = F'table-{self.env.new_serialno("table")}'
+        node.table_id = f'table-{self.env.new_serialno("table")}'
         node.settings = settings
 
-        with open(settings['csv_file']) as f:
+        with open(settings["csv_file"]) as f:
             reader = csv.reader(f)
 
             for count, row in enumerate(reader):
@@ -39,18 +40,19 @@ class DataTable(SphinxDirective):
                     node.data.append(row)
         return [node]
 
+
 def html_visit_output_node(self, node):
 
     # Build HTML table
 
     table_string = ""
 
-    table_string += F"<table id='{node.table_id}' class='display'>\n"
+    table_string += f"<table id='{node.table_id}' class='display'>\n"
     table_string += "\t<thead>\n\t\t<tr>\n"
 
     for header in node.headers:
-        table_string += F"\t\t\t<th>{header}</th>\n"
-    
+        table_string += f"\t\t\t<th>{header}</th>\n"
+
     table_string += "</tr></thead>\n<tbody>\n"
 
     for row in node.data:
@@ -58,11 +60,11 @@ def html_visit_output_node(self, node):
         for entry in row:
             try:
                 numeric_entry = float(entry)
-                entry = F"{numeric_entry:.3f}"
+                entry = f"{numeric_entry:.3f}"
             except ValueError:
                 entry = entry
 
-            table_string += F"\t\t\t<td>{entry}</td>\n"
+            table_string += f"\t\t\t<td>{entry}</td>\n"
         table_string += "\t</tr>\n"
 
     table_string += "\t</tbody>"
@@ -70,9 +72,10 @@ def html_visit_output_node(self, node):
 
     self.body.append(table_string)
 
+
 def html_depart_output_node(self, node):
 
-    initialize_table_script = F"""<script>
+    initialize_table_script = f"""<script>
 
     $(document).ready( function () {{
             $('#{node.table_id}').DataTable({{"ordering": false, "paging": false}});}} );
@@ -83,24 +86,17 @@ def html_depart_output_node(self, node):
     self.body.append(initialize_table_script)
 
 
-
-
-
 def setup(app):
 
     app.add_node(
         data_table,
-        html=(
-            html_visit_output_node,
-            html_depart_output_node
-        ),
+        html=(html_visit_output_node, html_depart_output_node),
     )
 
     app.add_directive("datatable", DataTable)
 
     return {
-        'version': '0.1',
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
-
--- docs/_ext/moleculeView.py
+++ docs/_ext/moleculeView.py
@@ -11,22 +11,24 @@ from sphinx.util.docutils import SphinxDirective
 class view(nodes.General, nodes.Element):
     pass
 
+
 class MoleculeView(SphinxDirective):
 
     has_content = True
 
     def run(self):
-        view_id = F'viewport-{self.env.new_serialno("view")}' 
+        view_id = f'viewport-{self.env.new_serialno("view")}'
         settings = {}
 
         for content in self.content:
-            split_content = content.split(':', 1)
-            settings[split_content[0] ] = split_content[1]
+            split_content = content.split(":", 1)
+            settings[split_content[0]] = split_content[1]
 
         node = view()
         node.view_id = view_id
         node.settings = settings
         return [node]
+
 
 def html_visit_output_node(self, node):
 
@@ -34,44 +36,45 @@ def html_visit_output_node(self, node):
         "data-backgroundcolor": "white",
         "width": "400px",
         "height": "300px",
-        "data-style": "stick"
+        "data-style": "stick",
     }
 
-    if 'data-pdb' not in node.settings and 'data-href' not in node.settings:
+    if "data-pdb" not in node.settings and "data-href" not in node.settings:
         raise KeyError("You must specify a path to a pdb or file to visualize.")
-    
-    if 'data-pdb' and 'data-href' in node.settings:
+
+    if "data-pdb" and "data-href" in node.settings:
         raise ValueError("You should only specify one molecule file to visualize.")
 
     for k, v in node.settings.items():
         k = k.strip()
         defaults[k] = v.strip()
 
-    source_string = ''
+    source_string = ""
 
-    if 'data-pdb' in defaults:
-        source_string = F'data-pdb={defaults["data-pdb"]}'
-    elif 'data-href' in defaults:
-        source_string = F'data-href={defaults["data-href"]}'
+    if "data-pdb" in defaults:
+        source_string = f'data-pdb={defaults["data-pdb"]}'
+    elif "data-href" in defaults:
+        source_string = f'data-href={defaults["data-href"]}'
 
     # Set data styles
-    data_style = ''
+    data_style = ""
 
     # Process extra styles
-    style_keys = [k for k in defaults.keys() if 'style' in k]
-    select_keys = [k for k in defaults.keys() if 'select' in k]
-    
+    style_keys = [k for k in defaults.keys() if "style" in k]
+    select_keys = [k for k in defaults.keys() if "select" in k]
+
     for i in range(len(style_keys)):
         style_key = style_keys[i]
-        data_style += F" {style_key}={defaults[style_key]} "
-    
+        data_style += f" {style_key}={defaults[style_key]} "
+
     for i in range(len(select_keys)):
         select_key = select_keys[i]
-        data_style += F" {select_key}={defaults[select_key]} "
+        data_style += f" {select_key}={defaults[select_key]} "
 
-    show_string = F'<center><div style="height: {defaults["height"]}; width: {defaults["width"]}; position: relative;" class="viewer_3Dmoljs" {source_string} data-backgroundcolor="{defaults["data-backgroundcolor"]}" {data_style}></div></center>'
+    show_string = f'<center><div style="height: {defaults["height"]}; width: {defaults["width"]}; position: relative;" class="viewer_3Dmoljs" {source_string} data-backgroundcolor="{defaults["data-backgroundcolor"]}" {data_style}></div></center>'
 
     self.body.append(show_string)
+
 
 def html_depart_output_node(self, node):
     pass
@@ -81,17 +84,13 @@ def setup(app):
 
     app.add_node(
         view,
-        html=(
-            html_visit_output_node,
-            html_depart_output_node
-        ),
+        html=(html_visit_output_node, html_depart_output_node),
     )
 
     app.add_directive("moleculeview", MoleculeView)
 
     return {
-        'version': '0.1',
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
-
