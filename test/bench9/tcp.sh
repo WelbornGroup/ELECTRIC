ee57ea25acbcaf2981dc162a62e2531e31d7d409
Jessica Nash <janash@vt.edu>
2023-12-03 00:07:55 -0500

    add another benchmark for testing

---
+++ test/bench9/tcp.sh
@@ -0,0 +1,28 @@
+#location of required codes
+DRIVER_LOC=$(cat ../locations/ELECTRIC)
+TINKER_LOC=$(cat ../locations/Tinker_ELECTRIC)
+
+#remove old files
+if [ -d work ]; then
+  rm -r work
+fi
+
+#create work directory
+cp -r data work
+cd work
+
+#set the number of threads
+export OMP_NUM_THREADS=1
+
+# Run the tinker md simulation
+# This will print out 5 frames to traj_out.arc
+# A copy of traj_out.arc is included in data/, so it isn't necessary to re-run this
+# rm bench5.arc; ${TINKER_LOC} bench5 -k bench5.key 10 1.0 0.001999 2 300.00 > Dynamics.log
+
+#launch Tinker using EWALD
+${TINKER_LOC} bench9 -k no_ewald.key -mdi "-role ENGINE -name NO_EWALD -method TCP -port 8022 -hostname localhost" 10 1.0 0.001999 2 300.00 > no_ewald.log &
+
+#launch driver
+python -m cProfile -o profile.prof ${DRIVER_LOC}  -probes "1 40" -snap bench9.xyz -mdi "-role DRIVER -name driver -method TCP -port 8022" --bymol &
+
+wait
